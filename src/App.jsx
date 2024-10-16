29e035f877396b2ee2a0f24fa05d40eaca96562d
Raul Magdadaro Jr <magsasaka9599@gmail.com>
2024-10-17 00:05:59 +0800

    Dashboard page with sidebar navigation

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,24 @@
-import Form from 'react-bootstrap/Form';
-import Button from 'react-bootstrap/Button';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import Registration from './pages/Registration';
 import Login from './pages/Login';
+import Dashboard from './components/Dashboard';
+import Users from './components/Users';
+import Products from './components/Products';
 
 function AppFunction() {
   return (
-    <div>
-      <h1>REGISTRATION</h1>
-      <Registration/>
-      <h1>LOGIN</h1>
-      <Login/>
-    </div>
+    <Router>
+      <Routes>
+        <Route path= "/" element={<Navigate to="/login"/>}/>
+        <Route path="/login" element={<Login />} />
+        <Route path="/registration" element={<Registration />} />
+        <Route path="/dashboard" element={<Dashboard />}>
+          <Route path="users" element={<Users />} />
+          <Route path="products" element={<Products />} />
+        </Route>
+      </Routes>
+    </Router>
   );
 }
 
