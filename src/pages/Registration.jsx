438ec33f52c7c42abc04039db773460653d8a7b4
Raul Magdadaro Jr <magsasaka9599@gmail.com>
2024-10-16 23:52:12 +0800

    change on the login

---
+++ src/pages/Registration.jsx
@@ -0,0 +1,53 @@
+import Button from 'react-bootstrap/Button';
+import Col from 'react-bootstrap/Col';
+import Form from 'react-bootstrap/Form';
+import Row from 'react-bootstrap/Row';
+
+function Registration() {
+  return (
+    <Form>
+      <Row className="mb-3">
+        <Form.Group as={Col} controlId="formGridUsername">
+          <Form.Label>Username</Form.Label>
+          <Form.Control type="text" placeholder="Username" />
+        </Form.Group>
+
+        <Form.Group as={Col} controlId="formGridPassword">
+          <Form.Label>Password</Form.Label>
+          <Form.Control type="password" placeholder="Password" />
+        </Form.Group>
+      </Row>
+
+      <Form.Group className="mb-3" controlId="formGridAddress1">
+        <Form.Label>Address</Form.Label>
+        <Form.Control placeholder="1234 Main St" />
+      </Form.Group>
+
+      <Row className="mb-3">
+        <Form.Group as={Col} controlId="formGridCity">
+          <Form.Label>City</Form.Label>
+          <Form.Control />
+        </Form.Group>
+
+        <Form.Group as={Col} controlId="formGridState">
+          <Form.Label>State</Form.Label>
+          <Form.Select defaultValue="Choose...">
+            <option>Choose...</option>
+            <option>...</option>
+          </Form.Select>
+        </Form.Group>
+
+        <Form.Group as={Col} controlId="formGridZip">
+          <Form.Label>Zip</Form.Label>
+          <Form.Control />
+        </Form.Group>
+      </Row>
+
+      <Button variant="primary" type="submit">
+        Register
+      </Button>
+    </Form>
+  );
+}
+
+export default Registration;
